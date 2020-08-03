b0d8878eeffaee6b4bea8320ce1bb715689221b4
Nate Fallowfield <nfallowf@uwo.ca>
2020-08-03 15:08:44 -0400

    Basic stock data manipulations

---
+++ plotHelpers.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')  
+
+# Plot closing price
+def plotData(data):
+    data.plot()
+    plt.show()
+    plt.close()
+    
+# Plot closing price with Rolling Average
+def plotRA(data, RA):
+    data.plot()
+    RA.plot()
+    plt.show()
+    plt.close()
+
+# Plot closing price with volume traded
+def plotVolume(data):
+    ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+    ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+    ax1.plot(data.index, data['Close'])
+    ax2.bar(data.index, data['Volume'])
+    plt.show()
+    plt.close()
